b5787b59933db3b9c233c4608de5ac67c4624655
Pawel O2 <Tvrixpe@gmail.com>
2025-04-06 14:50:53 +0200

    add 3.1.py

---
+++ 3.1.py
@@ -0,0 +1,25 @@
+#Zadanie 1
+
+Sklepy = {
+    "piekarnia": ["Chleb", "Pączek", "Bułki"],
+    "warzywniak": ["Marchew", "Seler", "Rukola"]
+}
+
+for i in Sklepy.keys():
+
+    print (f"Idę do {(i.capitalize())} i kupuję tam {Sklepy[i]} ")
+    r = Sklepy.get(i)
+    X = r+r
+
+    
+print (f"W sumie kupuję {(len(X))} produktów")
+
+
+#Zadanie 2
+Zbior = []
+for i in range (101):
+    if i%5 ==0:
+        Zbior.append(i)
+print (Zbior)
+dop = [Zbior**3 for Zbior in Zbior]
+print (dop)
